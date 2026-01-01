3a26d3a409ec5cf9a771465f45f01cedba666b6e
amarkwong <amark_wong@hotmail.com>
2026-01-01 19:16:25 +1000

    wip

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Arcade from "@/components/Arcade/Arcade";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center bg-[rgb(var(--background-rgb))]">
+      <Arcade />
+    </main>
+  );
+}
---
+++ apps/web/src/db/schema.ts
@@ -0,0 +1,8 @@
+import { time } from "console";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: serial('id').primaryKey(),
+    name: text('name'),
+    created_at: timestamp('created_at').notNull().defaultNow(),
+});
